274e4e4cd4c643238c511ea9ed22cf6658d909ed
Roza <rozaogannesyan109@gmail.com>
2023-10-02 15:31:14 +0300

    arows

--- wp-content/themes/example/single-restaurant.php
+++ wp-content/themes/example/single-restaurant.php
@@ -54,10 +54,21 @@ get_header(); ?>
 
             </div>
             <div class="limonchello__swiper-button-prev">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/prevv.svg" alt="" />
+                <svg data-v-9e081b70="" width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="stocks__button__icon">
+                    <g data-v-9e081b70="" clip-path="url(#clip0_1836_1028)">
+                        <path data-v-9e081b70="" d="M16 17.6416L8.5799 10L16 2.35836L13.7101 -1.00095e-07L4 10L13.7101 20L16 17.6416Z" fill="#23262F"></path>
+                    </g>
+                    <defs data-v-9e081b70="">
+                        <clipPath data-v-9e081b70="" id="clip0_1836_1028">
+                            <rect data-v-9e081b70="" width="20" height="20" fill="white" transform="translate(20) rotate(90)"></rect>
+                        </clipPath>
+                    </defs>
+                </svg>
             </div>
             <div class="limonchello__swiper-button-next">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/nextt.svg" alt="" />
+                <svg data-v-9e081b70="" width="16" height="16" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="stocks__button__icon">
+                    <path data-v-9e081b70="" d="M2.10374e-07 2.35836L7.4201 10L2.81232e-08 17.6416L2.28991 20L12 10L2.28991 2.73069e-08L2.10374e-07 2.35836Z" fill="#23262F"></path>
+                </svg>
             </div>
         </div>
         <div class="container popup-content">
