7f642ada07cdef399a4a84ec20ba6ae2be2826ab
Roza <rozaogannesyan109@gmail.com>
2023-10-02 15:33:42 +0300

    arrows

--- wp-content/themes/example/front-page.php
+++ wp-content/themes/example/front-page.php
@@ -31,8 +31,9 @@ get_header(); ?>
                       </clipPath>
                     </defs>
                   </svg>
-                </div>xkxoHstw
-                abetadev
+                </div>
+                <!--xkxoHstw
+                abetadev-->
                 <div class="slider__swiper-button-next">
                   <svg data-v-9e081b70="" width="12" height="20" viewBox="0 0 12 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="stocks__button__icon">
                     <path data-v-9e081b70="" d="M2.10374e-07 2.35836L7.4201 10L2.81232e-08 17.6416L2.28991 20L12 10L2.28991 2.73069e-08L2.10374e-07 2.35836Z" fill="#23262F"></path>
