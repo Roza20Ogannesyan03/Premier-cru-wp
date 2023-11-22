f5198f6106a975e04befff078cd8157513fe08c9
Roza <rozaogannesyan109@gmail.com>
2023-11-22 18:00:52 +0300

    sf\

--- wp-content/themes/example/templates/o-kompanii.php
+++ wp-content/themes/example/templates/o-kompanii.php
@@ -14,7 +14,7 @@ get_header(); ?>
 <div class="background-img">
     <img style="width: 100%" src="<?php echo get_template_directory_uri(); ?>/assets/images/background.png" alt="" />
 </div>
-<div class="container-large">
+<div class="container-small">
     <ul class="breadcrumb">
 
         <?php if (function_exists('bcn_display')) {
